aefa6bc58573aadd716d34f1d728304d2ed8c415
RustamNigmatullin <nigmatullinrr@gmail.com>
2024-03-31 11:16:46 +0700

    Created base project, removed storyboard references

---
+++ FoodDelieveryApp/FoodDelieveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelieveryApp
+//
+//  Created by Rustam Nigmatullin on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
